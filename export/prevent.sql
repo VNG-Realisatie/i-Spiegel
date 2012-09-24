adfa2f7e5b357f64c88938fd76ea889a4675fc81
eywitteveen@gmail.com <eywitteveen@gmail.com@258fc368-26be-1d28-eb37-1f6fdbcd83b9>
2012-09-24 11:12:30 +0000

    huidige adres view

--- export/prevent.sql
+++ export/prevent.sql
@@ -10,3 +10,9 @@ DROP MATERIALIZED VIEW dl_prevent_placestreet;
 CREATE MATERIALIZED VIEW dl_prevent_placestreet
 AS SELECT *
 FROM PREVENT.PLACE_STREET@prev;
+
+CREATE MATERIALIZED VIEW prevent_huidige_adres 
+AS SELECT * 
+FROM dl_prevent_address ADDRESS
+JOIN dl_prevent_placestreet PLACE_STREET
+ON ADDRESS.PLACE_STREET_ID = PLACE_STREET.ID_PLACE_STREET;
