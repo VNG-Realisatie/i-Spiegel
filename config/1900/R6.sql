f8efdb8cc0055e92389349bcb5661f7872f59ae1
eywitteveen@gmail.com <eywitteveen@gmail.com@258fc368-26be-1d28-eb37-1f6fdbcd83b9>
2013-04-23 14:19:42 +0000

    feedback van GBA

--- config/1900/R6.sql
+++ config/1900/R6.sql
@@ -18,3 +18,23 @@ R6:
 	Huisnummertoevoeging*	
 	Datum overlijden*
 */
+SELECT gba_vselarcp.DATUM_OVERLIJDEN,
+  gba_vselarcp.ADMINISTRATIENUMMER,
+  gba_vselarcp.BSN,
+  gba_vselarcp.VOORVOEGSEL,
+  gba_vselarcp.GESLACHTSNAAM,
+  gba_vselarcp.GEBOORTEDATUM,
+  gba_vselarcp.GEBOORTEPLAATSNAAM,
+  gba_vselarcp.KODE_GESLACHT,
+  gba_vselarcp.POSTKODE_NUMERIEK,
+  gba_vselarcp.POSTKODE_ALFANUMERIEK,
+  gba_vselarcp.STRAATNAAM,
+  gba_vselarcp.STRAATNAAM_ZD,
+  gba_vselarcp.STRAATNAAM_TLX,
+  gba_vselarcp.HUISNUMMER,
+  gba_vselarcp.HUISLETTER,
+  gba_vselarcp.TOEVOEGING_HUISNUMMER,
+  gba_vselarcp.AANDUIDING_HUISNUMMER,
+  gba_vselarcp.GEMEENTEKODE_INSCHRIJVING
+FROM gba_vselarcp@PPIV
+WHERE NOT gba_vselarcp.DATUM_OVERLIJDEN IS NULL
