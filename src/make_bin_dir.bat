62eb7aeecedfe190f8e033b93cf57aab4d3683f7
Eduard Witteveen <e.witteveen@sudwestfryslan.nl>
2019-05-17 10:39:29 +0200

    - De i-Spiegel/bin/ directory gerefactored: nu een 32 en 64 bits versie - Het proces om te komen tot de release wat aangepast - Files die bij oplevering horen nu in i-Spiegel/templates/

--- src/make_bin_dir.bat
+++ src/make_bin_dir.bat
@@ -1,5 +1,24 @@
-copy *.accdb ..\bin\
-copy *.csv ..\bin\
-copy bin\x86\Release\ISpiegel.exe ..\bin\ISpiegel.exe
-copy bin\x64\Release\ISpiegel.exe ..\bin\ISpiegel64.exe
-copy ..\bin\ISpiegel.exe.config ..\bin\ISpiegel64.exe.config
+echo We gaan dingen verwijderen en copieren, weet je het zeker, druk dan op een knop! Zoniet, sluit dit venster dan maar snel!
+pause
+
+del /P /F /S /Q ..\bin\ISpiegelx86\*.*
+del /P /F /S /Q ..\bin\ISpiegelx86
+del /P /F /S /Q ..\bin\ISpiegelx64\*.*
+del /P /F /S /Q ..\bin\ISpiegelx64
+
+mkdir ..\bin\ISpiegelx86
+mkdir ..\bin\ISpiegelx64
+
+copy ..\templates\*.accdb ..\bin\ISpiegelx86
+copy ..\templates\*.accdb ..\bin\ISpiegelx64
+copy ..\templates\*.csv ..\bin\ISpiegelx86
+copy ..\templates\*.csv ..\bin\ISpiegelx64
+
+copy ISpiegel\bin\x86\Release\ISpiegel.exe ..\bin\ISpiegelx86\ISpiegel.exe
+copy ISpiegel\bin\x64\Release\ISpiegel.exe ..\bin\ISpiegelx64\ISpiegel64.exe
+
+copy ..\templates\ISpiegel.exe.config ..\bin\ISpiegelx86\ISpiegel.exe.config
+copy ..\templates\ISpiegel.exe.config ..\bin\ISpiegelx64\ISpiegel64.exe.config
+
+copy ISpiegel\bin\x86\Release\*.dll ..\bin\ISpiegelx86\
+copy ISpiegel\bin\x64\Release\*.dll ..\bin\ISpiegelx64\
