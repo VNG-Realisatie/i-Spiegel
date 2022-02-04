48f53de34708b65a4b80f5f4c1ef00c206a78478
Eduard Witteveen <eywitteveen@gmail.com>
2022-02-04 14:31:17 +0100

    update to visual studio 2022 update to .net 4.8 settingsfile in sync

--- src/ISpiegel/Properties/Settings.Designer.cs
+++ src/ISpiegel/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ISpiegel.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -61,6 +61,15 @@ namespace ISpiegel.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("System.Data.OleDb")]
+        public string databaseprovider {
+            get {
+                return ((string)(this["databaseprovider"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("ISPIEGEL_")]
         public string databaseprefix {
             get {
@@ -79,18 +88,27 @@ namespace ISpiegel.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("demo:demo")]
-        public string influxdb_auth {
+        [global::System.Configuration.DefaultSettingValueAttribute("html")]
+        public string output_format {
             get {
-                return ((string)(this["influxdb_auth"]));
+                return ((string)(this["output_format"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool escape_logging {
+            get {
+                return ((bool)(this["escape_logging"]));
             }
         }
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=db01\\exact2003;Initial Catalog=ispiegel;User Id=ispiegel;Password=isp" +
-            "iegel;")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=${WORKING_DIRECTORY}\\ISpiegel.accdb" +
+            "")]
         public string databaseconnection {
             get {
                 return ((string)(this["databaseconnection"]));
@@ -99,19 +117,19 @@ namespace ISpiegel.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("System.Data.SqlClient")]
-        public string databaseprovider {
+        [global::System.Configuration.DefaultSettingValueAttribute("http://digimelden.nl/write?db=ispiegel")]
+        public string influxdb_url {
             get {
-                return ((string)(this["databaseprovider"]));
+                return ((string)(this["influxdb_url"]));
             }
         }
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string influxdb_url {
+        [global::System.Configuration.DefaultSettingValueAttribute("ispiegel:ispiegel")]
+        public string influxdb_auth {
             get {
-                return ((string)(this["influxdb_url"]));
+                return ((string)(this["influxdb_auth"]));
             }
         }
     }
